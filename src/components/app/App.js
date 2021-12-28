5b2bed2b47e881d395fc538e42e243c6951bb15d
Logunovich <logunovich@gmail.com>
2021-12-28 18:57:08 +0100

    commit with hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 import AppHeader from "../appHeader/AppHeader.js";
 import RandomChar from "../randomChar/RandomChar.js";
 import CharList from "../charList/CharList.js";
@@ -7,34 +7,29 @@ import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 import './app.css';
 
 
-class App extends Component {
-    state = {
-        selectedChar: null
+const App = () => {
+
+    const [selectedChar, setChar] = useState(null);
+
+    const onCharSelected = (id) => {
+        setChar(id);
     }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <AppHeader/>
-                <RandomChar/>
-                <div className="main__block">
-                    <CharList 
-                        charId={this.state.selectedChar}
-                        onCharSelected={this.onCharSelected}/>
-                    <ErrorBoundary>
-                        <CharInfo charId={this.state.selectedChar}/>
-                    </ErrorBoundary>
-                    
-                </div>
+    return (
+        <div className="container">
+            <AppHeader/>
+            <RandomChar/>
+            <div className="main__block">
+                <CharList 
+                    charId={selectedChar}
+                    onCharSelected={onCharSelected}/>
+                <ErrorBoundary>
+                    <CharInfo charId={selectedChar}/>
+                </ErrorBoundary>
+                
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default App;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,63 +1,54 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './CharList.css';
 import CharListItem from '../charListItem/CharListItem';
 import MarvelService from '../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 
-class CharList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            arr: [],
-            loading: true,
-            newItemLoading: false,
-            offset: 210
-        }
-    }
-    
+const CharList = (props) => {
 
-    marvelService = new MarvelService();
+    const [arr, setArr] = useState([]),
+          [loading, setLoading] = useState(true),
+          [newItemLoading, setNewItemLoading] = useState(false),
+          [offset, setOffset] = useState(210);    
 
-    getCharacters = (newArr) => {
-        this.setState(({arr, offset}) => ({
-            arr: [...arr, ...newArr],
-            loading: false,
-            newItemLoading: false,
-            offset: offset + 9
-        }))
+    const marvelService = new MarvelService();
+
+    const getCharacters = (newArr) => {
+        setArr(arr => [...arr, ...newArr]);
+        setLoading(false);
+        setNewItemLoading(false);
+        setOffset(offset => {
+            return offset + 9
+        });
     }
 
-    componentDidMount() {
-        this.onRequest();
-    }
 
-    onRequest = (offset) => {
-        this.onCharListLoading();
-        this.marvelService
+    useEffect(() => {
+        onRequest()
+    }, []);
+
+    const onRequest = (offset) => {
+        onCharListLoading();
+        marvelService
             .getAllCharacters(offset)
-            .then(this.getCharacters)
+            .then(getCharacters)
             .catch(() => console.log('error'))
     }
 
-    onCharListLoading() {
-        this.setState({
-            newItemLoading: true
-        })
+    const onCharListLoading = () => {
+        setNewItemLoading(true);
     }
 
-
-    render() {
-        const {arr, newItemLoading, offset} = this.state;
         let notFoundImg = ' img__not-found';
-        const loading = this.state.loading ? <Spinner/> : null;
+        const isLoading = loading ? <Spinner/> : null;
 
         const charItems = arr.map(item => {
             return (
                 <div key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => props.onCharSelected(item.id)}>
                     <CharListItem
                         id={item.id}
-                        curId={this.props.charId}
+                        curId={props.charId}
                         name={item.name.length>25?`${item.name.substr(0, 25)}...`:item.name}
                         img={item.thumbnail}
                         classImg={item.thumbnail.includes('not_available')?notFoundImg:''}
@@ -68,19 +59,18 @@ class CharList extends Component {
         })
         return (
             <div className="char_list">
-                {loading}
+                {isLoading}
                 <div className="char__list_container">
                     {charItems}
                 </div>
                 <button 
                     className="randomchar__container_btn randomchar__container_btn-load-more"
                     disabled={newItemLoading}
-                    onClick={() => this.onRequest(offset)}>
+                    onClick={() => onRequest(offset)}>
                         Load more</button>
             </div>
             
         )
-    }    
-}
+    }   
 
 export default CharList;
