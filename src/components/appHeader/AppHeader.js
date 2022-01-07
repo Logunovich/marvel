589fc7fb620bff6e101bc7b4cb97ae9264713d0b
Logunovich <logunovich@gmail.com>
2022-01-07 14:08:15 +0100

    fixed bugs with navlink

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -10,7 +10,7 @@ const AppHeader = () => {
             <div className="app__container_right">
                 {/* <a href="https:r24.by/" className="app__container-active">Characters</a> / <a href="https:r24.by/">Comics</a></div> */}
                 <NavLink end className={({isActive}) => isActive ? 'app__container-active' : ''} to="/">Characters</NavLink> / 
-                <NavLink end className={({isActive}) => isActive ? 'app__container-active' : ''} to="/comics">Comics</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'app__container-active' : ''} to="/comics">Comics</NavLink>
             </div>
         </div>
     )
